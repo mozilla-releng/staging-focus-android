2ea2c77a5048456cc70719e072ca64e2eb16c20a
Sarah Clements <sclements313@gmail.com>
2021-09-29 18:04:14 +0100

    add klar and focus release changes

--- taskcluster/focus_android_taskgraph/target_tasks.py
+++ taskcluster/focus_android_taskgraph/target_tasks.py
@@ -11,8 +11,15 @@ def target_tasks_default(full_task_graph, parameters, graph_config):
     """Target the tasks which have indicated they should be run on this project
     via the `run_on_projects` attributes."""
 
-    filter = filter_for_tasks_for
-    return [l for l, t in full_task_graph.tasks.items() if filter_for_tasks_for(t, parameters)]
+    def releases(task, parameters):
+        return task.attributes.get("release-type", "")
+
+    _filter = filter_for_tasks_for
+
+    if parameters["tasks_for"] == "github-release":
+        _filter = releases
+
+    return [l for l, t in full_task_graph.tasks.items() if _filter(t, parameters)]
 
 
 @_target_task('release')
